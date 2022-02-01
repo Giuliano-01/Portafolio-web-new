7131b11e9fc43dfb220820fa035ec4b13a510b47
Giuliano De Palma <giulianodepalma@outlook.com>
2022-02-01 20:02:03 -0300

    visual details

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    /*poner scroll en el inicio*/
+    window.scrollTo(0,0);
+
     $('.habilidades-Menu').on('click',function(){
         window.scrollTo(0, $('.habilidades-container').offset().top);
     });
@@ -7,5 +10,29 @@ $(document).ready(function(){
         window.scrollTo(0, $('.proyectos-container').offset().top);
     });
 
+    /*animations on load*/
+    if(window.scrollY==0){
+        $('.banner-content h1').animate({opacity:0},'1500',function(){console.log("termino")});
+        $('.banner-content h1').animate({opacity:1},'slow',function(){console.log("termino")});
+    }
+    $(document).on('scroll',function (){
+        if(window.scrollY==0){
+            $('.banner-content h1').animate({opacity:1},'slow',function(){console.log("termino")});
+        }      
+        console.log(window.scrollY);
+    });
+    
+    $('.more-info-link-container').on('click', '.flechaabajo',function(event){
+        event.preventDefault();
+        if($('.more-info-link-container').children().hasClass("flechaarriba")){
+            window.scrollTo(0, $('.cabecera').offset().top);
+        }else{
+            window.scrollTo(0, $('.habilidades-container').offset().top);
+        }
+        
+        $(this).closest('.habilidades-container').find('.moreinfo').toggle(500, function(){console.log("se completo")}); //tarda 1 seg en mostrarse y el otro param es la funcion de cuando termina
+        $('.more-info-link-container').children().toggleClass('flechaarriba');
+        
+    });
 
 });
